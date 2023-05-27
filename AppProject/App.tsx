f8cbfaaf54fd64247e9a1caa749fe03bb306fbe6
rashadma <rashadkhm@code.edu.az>
2023-05-27 14:18:06 +0400

    Hehe

---
+++ AppProject/App.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        Ne vaxt gedirik ?
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
